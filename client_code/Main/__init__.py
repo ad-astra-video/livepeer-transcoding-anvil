08bc2c457f3deda0549d9e634d7aa13eed5cdb0a
0xb79orch@gmail.com <0xb79orch@gmail.com>
2023-11-05 02:40:28 +0000

    Created Main, Main.FileInfo, Main.JobProfiles, Main.JobsInProgress and 4 more; Edited settings; Deleted Form1

---
+++ client_code/Main/__init__.py
@@ -0,0 +1,22 @@
+from ._anvil_designer import MainTemplate
+from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+import anvil.server
+from .NewJob import NewJob
+class Main(MainTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+    while not anvil.users.login_with_form():
+      pass
+
+  def form_show(self, **event_args):
+    """This method is called when the form is shown on the page"""
+    self.content_panel.add_component(NewJob())
