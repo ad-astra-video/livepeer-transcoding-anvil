c8894e298a8ed7bcf04e90ecc533c9b2cfd6fa57
0xb79orch@gmail.com <0xb79orch@gmail.com>
2023-11-07 03:59:50 +0000

    Edited Upload

--- server_code/Upload.py
+++ server_code/Upload.py
@@ -8,7 +8,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
-with os
+import os, boto3
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
